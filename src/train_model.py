94900363526346a24b0fd2acb3052cd401774e96
Osman Osman <osma@student.chalmers.se>
2021-07-02 21:27:52 +0200

    Train Linear Regression

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,7 @@
 import pickle
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 
 from config import Config
 
@@ -8,10 +9,13 @@ Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
 
 X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
-
+"""
 model = RandomForestRegressor(
     n_estimators=150, max_depth=6, random_state=28750
 )
+"""
+model = LinearRegression()
+
 model = model.fit(X_train, y_train.to_numpy().ravel())
 
 pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
