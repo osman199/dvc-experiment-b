b1da3b58cd91636bc15396f8419bdedbd3222ec9
Osman Osman <44290264+osman199@users.noreply.github.com>
2021-07-18 15:31:26 +0200

    Update prepare.py

--- src/prepare.py
+++ src/prepare.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_boston, fetch_california_housing
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import yaml
 
 from config import Config
 
@@ -8,17 +9,18 @@ from config import Config
 Config.ORIGINAL_DATASET_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
 Config.DATASET_PATH.mkdir(parents=True, exist_ok=True)
 
+
+params = yaml.safe_load(open('params.yaml'))['prepare']
+
 load_boston(str(Config.ORIGINAL_DATASET_FILE_PATH))
 data = load_boston()
 df = pd.DataFrame(data=data['data'], columns = data['feature_names'])
 df['MEDV'] = data.target
-# print(df.head)
+
 df.to_csv(str(Config.ORIGINAL_DATASET_FILE_PATH), sep = ',', index = False)
 
 
-df_train, df_test = train_test_split( df, test_size=0.2, random_state=28750 )
-
-#df = pd.read_csv(str(Config.ORIGINAL_DATASET_FILE_PATH))
+df_train, df_test = train_test_split( df, test_size=params["split"], random_state=params["seed"] )
 
 df_train.to_csv(str(Config.DATASET_PATH / "train.csv"), index=None)
 df_test.to_csv(str(Config.DATASET_PATH / "test.csv"), index=None)
