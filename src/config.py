173c1671094ebcaf85c56aa0e510d7d3e5801188
Osman Osman <44290264+osman199@users.noreply.github.com>
2021-07-20 01:37:04 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 class Config:
     ASSETS_PATH = Path("./data")
-    ORIGINAL_DATASET_FILE_PATH = ASSETS_PATH / "raw" / "boston.csv"
+    ORIGINAL_DATASET_FILE_PATH = ASSETS_PATH / "raw"
     DATASET_PATH = ASSETS_PATH / "prepared"
     FEATURES_PATH = ASSETS_PATH / "features"
     MODELS_PATH =  Path("./model")
