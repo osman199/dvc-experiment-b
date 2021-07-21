f5be9d5a76fb849f2f61c7d19f5b0c0698bd9674
Osman Osman <44290264+osman199@users.noreply.github.com>
2021-07-21 17:29:09 +0200

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,7 @@
 import pickle
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 import yaml
 
 from config import Config
@@ -12,9 +13,9 @@ params = yaml.safe_load(open('params.yaml'))['train']
 X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
 
-model = RandomForestRegressor(
-    n_estimators=params["n_estimators"], max_depth=params["max_depth"], random_state=params["seed"], ccp_alpha=params["ccp_alpha"]
-)
+model = LinearRegression(normalize=True)
+#model = RandomForestRegressor()
+
 model = model.fit(X_train, y_train.to_numpy().ravel())
 
 pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
